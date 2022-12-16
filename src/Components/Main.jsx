9d4166201d790c73e36277f35b5a7e18c29516bb
TartCodes <tartcodes@gmail.com>
2022-12-16 15:21:31 -0500

    typos

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,9 +3,9 @@ const Main = () => {
     <main>
       <h2>About</h2>
       <p>
-        I am a passionate Full-Stack Developer looking to join a team of like
-        minded developers where I can share my skills, learn from others, and
-        grow in an ever evolving industry!
+        I am a passionate Full-Stack Developer looking to join a team of
+        like-minded developers where I can share my skills, learn from others,
+        and grow in an ever-evolving industry!
       </p>
       <h2>Interests</h2>
       <p>Coding. Food. Bourbon. Gaming. DOGS. In no particular order.</p>
